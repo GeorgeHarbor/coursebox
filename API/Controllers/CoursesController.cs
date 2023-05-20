3870c2407bd3a9c336b2e1a285034f45206426ff
GeorgeHarbor <ldjordjevic@protonmail.com>
2023-05-20 20:39:21 +0200

    Add List functionality

---
+++ API/Controllers/CoursesController.cs
@@ -0,0 +1,18 @@
+﻿using Application.Courses;
+using Domain;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Persistence;
+
+namespace API.Controllers;
+
+public class CoursesController : BaseApiController
+{
+    [HttpGet]
+    public async Task<List<Course>> Test()
+    {
+        return await Mediator.Send(new List.Query());
+    }
+}
---
+++ Application/Courses/List.cs
@@ -0,0 +1,32 @@
+﻿using Domain;
+using MediatR;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using Persistence;
+
+namespace Application.Courses;
+
+public class List
+{
+    public class Query : IRequest<List<Course>>
+    {
+        
+    }
+    
+    public class Handler : IRequestHandler<Query, List<Course>>
+    {
+        private readonly DataContext _context;
+
+        public Handler(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Course>> Handle(Query request, CancellationToken cancellationToken)
+        {
+            var result = await _context.Courses.FindSync(new BsonDocument()).ToListAsync(cancellationToken: cancellationToken);
+
+            return result;
+        }
+    }
+}
