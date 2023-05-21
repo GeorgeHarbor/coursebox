0d010a9b7f68cd8543a43899771923550eacad49
joves <tvojamama@gmail.com>
2023-05-21 15:09:33 +0200

    test

--- Domain/Course.cs
+++ Domain/Course.cs
@@ -17,6 +17,7 @@ public class Course
     public string Name { get; set; } = null!;
 
     [BsonElement("description")]
+
     public string Description { get; set; } = null!;
 
     [BsonElement("duration")]
