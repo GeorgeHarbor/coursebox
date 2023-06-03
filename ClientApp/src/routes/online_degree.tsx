4a6026b57e69ebe37a5c8a1de2b48628ba9f41c3
dsheedes <gvozdeniii@gmail.com>
2023-06-04 01:24:43 +0200

    online degree

--- ClientApp/src/routes/online_degree.tsx
+++ ClientApp/src/routes/online_degree.tsx
@@ -1,13 +1,14 @@
 import { Box } from '@chakra-ui/react'
 import { Navigation } from '../components/Navigation'
 import { Footer } from '../components/Footer'
+import { OnlineDegree } from '../components/OnlineDegree/OnlineDegree'
 
 export const OnlineDegreePage = () => (
-  <>
-    <Navigation />
-    <Box id="content">
-
-    </Box>
-    <Footer/>
+    <>
+        <Navigation />
+        <Box id="content" pt="64px">
+            <OnlineDegree />
+        </Box>
+        <Footer/>
     </>
 )
