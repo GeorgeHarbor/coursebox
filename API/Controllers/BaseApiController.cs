78e1d878994f3570de00b0abcbbe829386512043
GeorgeHarbor <ldjordjevic@protonmail.com>
2023-06-07 20:55:19 +0200

    Add HandleResult

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Net;
+using Application.Core;
+using Domain;
 using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
 
 namespace API.Controllers;
 
@@ -11,4 +14,16 @@ public class BaseApiController: ControllerBase
 {
         private IMediator _mediator;
         protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        protected ActionResult HandleResult<T>(Result<T> result)
+        {
+                
+                if(result == null)  return NotFound();
+                return result.IsSuccess switch
+                {
+                        true when result.Value != null => Ok(result.Value),
+                        true when result.Value == null => NotFound(),
+                        _ => BadRequest(result.Error)
+                };
+        }
 }
