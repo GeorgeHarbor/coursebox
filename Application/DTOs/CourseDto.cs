febcdcd720c2e7d3b216e2b916876d77046d33c3
GeorgeHarbor <ldjordjevic@protonmail.com>
2023-06-07 20:54:12 +0200

    Add DTOs

---
+++ Application/DTOs/CourseDto.cs
@@ -0,0 +1,18 @@
+﻿using Domain;
+
+namespace Application.DTOs;
+
+public class CourseDto
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public int? Duration { get; set; } = null!;
+    public string Image { get; set; }
+    public List<string> Keywords { get; set; }
+    public List<string> Instructors { get; set; }
+    public int? Rating { get; set; } = null!;
+    public object School { get; set; }
+    public string Level { get; set; }
+    public string Link { get; set; }
+}
---
+++ Application/DTOs/SchoolDto.cs
@@ -0,0 +1,12 @@
+﻿using Domain;
+
+namespace Application.DTOs;
+
+public class SchoolDto
+{
+    public string Id { get; set; }
+    public List<object> Courses { get; set; }
+    public string Description { get; set; }
+    public string ImageLink { get; set; }
+    public string Name { get; set; }
+}
